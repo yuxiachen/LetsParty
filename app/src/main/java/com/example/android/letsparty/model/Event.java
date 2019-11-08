68b91b494c81e8ba381ddacbefcac8088a08b926
Ying Wang <yingwang@Yings-MBP.T-mobile.com>
2019-11-07 22:57:16 -0800

    home page

--- app/src/main/java/com/example/android/letsparty/model/Event.java
+++ app/src/main/java/com/example/android/letsparty/model/Event.java
@@ -2,5 +2,20 @@ package com.example.android.letsparty.model;
 
 public class Event {
     private String category;
+    private String title;
+    private String time;
+    private String location;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public String getLocation(){
+        return location;
+    }
 
 }
--- app/src/main/java/com/example/android/letsparty/ui/TrendingFragment.java
+++ app/src/main/java/com/example/android/letsparty/ui/TrendingFragment.java
@@ -1,4 +1,82 @@
 package com.example.android.letsparty.ui;
 
-public class TrendingFragment {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.letsparty.R;
+import com.example.android.letsparty.adapter.EventListAdapter;
+import com.example.android.letsparty.model.Event;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class TrendingFragment extends Fragment implements EventListAdapter.OnEventItemClickedListener{
+
+    private RecyclerView recyclerView;
+    private EventListAdapter mAdapter;
+    private ArrayList<Event> resultEvents;
+    private ArrayList<String> eventKeys;
+
+    DatabaseReference dbEvents;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_trending, container);
+
+        recyclerView = view.findViewById(R.id.trending_event_list);
+        resultEvents = new ArrayList<>();
+        eventKeys = new ArrayList<>();
+        mAdapter = new EventListAdapter(resultEvents, eventKeys, this);
+        recyclerView.setAdapter(mAdapter);
+
+        dbEvents = FirebaseDatabase.getInstance().getReference("events");
+        dbEvents.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                resultEvents.clear();
+                eventKeys.clear();
+                for(DataSnapshot eventSnapshot : dataSnapshot.getChildren()){
+                    Event event = eventSnapshot.getValue(Event.class);
+                    String key = eventSnapshot.getKey();
+                    resultEvents.add(event);
+                    eventKeys.add(key);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+
+    private void openEventDetailActivity(String key){
+        Intent intent = new Intent(getActivity(), EventDetailActivity.class);
+        intent.putExtra("key", key);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void OnEventItemClicked(String key){
+        openEventDetailActivity(key);
+    }
 }
